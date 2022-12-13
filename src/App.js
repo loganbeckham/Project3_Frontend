8388ad3d846cf08c6937483eb890b71db2424de9
loganbeckham <loganbeckham1@gmail.com>
2022-12-13 16:08:34 -0600

    Added Show Routes for Attraction and Cleaner Navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 import DisplayCards from './components/DisplayCards'
+import Add from './components/add';
+import ShowCard from './components/ShowCard'
 
 function App() {
 
@@ -12,15 +14,6 @@ function App() {
 
     const [showCard, setShowCard] = useState(false)
     const [showLocation, setShowLocation] = useState([])
-    
-    //POST VARIABLES
-    const [newLocation, setNewLocation] = useState('')
-    const [newCity, setNewCity] = useState('')
-    const [newZip, setNewZip] = useState('')
-    const [newDescription, setNewDescription] = useState('')
-    const [newImage, setNewImage] = useState('')
-    const [newRating, setNewRating] = useState('')
-    const [newTags, setNewTags] = useState('')
 
     const cardDisplay = (props) => {
         setShowCard(!showCard)
@@ -29,60 +22,6 @@ function App() {
 
     const loggy = () => {
         console.log(showLocation)
-    }
-
-
-    // POST FORM FUNCTIONS
-    const handleNewLocation = (event) => {
-        setNewLocation(event.target.value)
-    }
-
-    const handleNewCity = (event) => {
-        setNewCity(event.target.value)
-    }
-
-    const handleNewZip = (event) => {
-        setNewZip(event.target.value)
-    }
-
-    const handleNewDescription = (event) => {
-        setNewDescription(event.target.value)
-    }
-
-    const handleNewImage = (event) => {
-        setNewImage(event.target.value)
-    }
-
-    const handleNewRating = (event) => {
-        setNewRating(event.target.value)
-    }
-
-    const handleNewTags = (event) => {
-        setNewTags(event.target.value)
-    }
-
-    // POST ROUTE
-    const handleNewLocationForm = (event) => {
-        event.preventDefault()
-        axios.post(
-            'https://project3-travelapp-backend.herokuapp.com/locations',
-            {
-                location: newLocation,
-                city: newCity,
-                zip: newZip,
-                description: newDescription,
-                image: newImage,
-                rating: newRating,
-                tags: newTags,
-            }
-        ).then(() => {
-            axios   
-                .get('https://project3-travelapp-backend.herokuapp.com/locations')
-                .then((response) => {
-                    setLocations(response.data)
-                })
-        })
-        event.target.reset()
     }
 
     // GET ROUTE
@@ -94,15 +33,10 @@ function App() {
             })
     })
 
-    // SHOW ROUTE
-    // const getShowLocation = () => {
-    //     axios
-    //         .get(`https://project3-travelapp-backend.herokuapp.com/locations${}`)
-    // }
-    
 
     // DELETE ROUTE
     const handleDelete = (locationData) => {
+        console.log(locationData._id)
         axios
             .delete(`https://project3-travelapp-backend.herokuapp.com/locations/${locationData._id}`)
             .then(() => {
@@ -112,6 +46,7 @@ function App() {
                         setLocations(response.data)
                     })
                 })
+        cardDisplay()
     }
 
 
@@ -128,65 +63,27 @@ function App() {
             <div>
                 { showCard ?
                 <>
-                <div>
-                    <div className='location-header'>
-                        <h1 className='ms-5 mt-4'>{showLocation.location}</h1>
-                        <div className='location-subheading'>
-                            <div>
-                                <p className='ms-5'>{showLocation.city} • {showLocation.rating} Stars</p>
-                            </div>
-                            <div>
-                                <p className='me-5'>Share • Save</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='location-body'>
-                        <div className='card' style={{width: '30vw'}}>
-                            <div className='card-body m-5'>
-                                <h5>Shared by ASDF</h5>
-                                <p className='card-text'>{showLocation.description}</p>
-                                
-                            </div>
-                            <p className='m-5'>{showLocation.tags}</p>        
-                        </div>
-                        <div className='location-image-container' >
-                            <img className='location-image ms-5 mt-3' style={{minHeight: '50vh'}} src={showLocation.image}/>
-                        </div>
-                    </div>
-                </div>
-
-                <button onClick={loggy}>
-                    loggy
-                </button>
-                <button onClick={cardDisplay}>back</button>
+                    <ShowCard showLocation={showLocation} setShowLocation={setShowLocation} cardDisplay={cardDisplay} handleDelete={handleDelete}/>
                 </>
                 :
-                <div className='row'>
-                    {locations.map((location) => {
-                        return (
-                            <>
-                                <DisplayCards location={location} setLocation={setLocations} handleDelete={handleDelete} cardDisplay={cardDisplay}/>
-                            </>
-                        )
-                    })}
-                </div>
+                <>
+                    <div className='row'>
+                        {locations.map((location) => {
+                            return (
+                                <>
+                                    <DisplayCards location={location} setLocation={setLocations} handleDelete={handleDelete} cardDisplay={cardDisplay}/>
+                                </>
+                            )
+                        })}
+                    </div>
+                    <button className="btn btn-primary m-5" data-bs-toggle="collapse" href={`#formSection`} aria-expanded="false" aria-controls={`#formSection`}>
+                        Add New Location
+                    </button>
+                    <Add setLocation={setLocations} />
+                </>
                 }
             </div>
-            <div>
-                <h1>Add New Location</h1>
-                <section>
-                    <form onSubmit={handleNewLocationForm}>
-                        location: <input type="text" onChange={handleNewLocation}/><br/>
-                        city: <input type="text" onChange={handleNewCity}/><br/>
-                        zip: <input type="number" onChange={handleNewZip}/><br/>
-                        description: <input type="text" onChange={handleNewDescription}/><br/>
-                        image: <input type="text" onChange={handleNewImage}/><br/>
-                        rating: <input type="range" min="0" max='5' onChange={handleNewRating}/><br/>
-                        tags: <input type="text" onChange={handleNewTags}/><br/>
-                        <input type="submit" value="Add Location"/>
-                    </form>
-                </section>
-            </div>
+            
         </>
     )
 }
--- src/components/add.js
+++ src/components/add.js
@@ -1,18 +1,83 @@
-import React from 'react'
+import {useState} from 'react'
+import axios from 'axios'
 
 const Add = (props) => {
+
+    const [newLocation, setNewLocation] = useState('')
+    const [newCity, setNewCity] = useState('')
+    const [newZip, setNewZip] = useState('')
+    const [newDescription, setNewDescription] = useState('')
+    const [newImage, setNewImage] = useState('')
+    const [newRating, setNewRating] = useState('')
+    const [newTags, setNewTags] = useState('')
+
+    // POST FORM FUNCTIONS
+    const handleNewLocation = (event) => {
+        setNewLocation(event.target.value)
+    }
+
+    const handleNewCity = (event) => {
+        setNewCity(event.target.value)
+    }
+
+    const handleNewZip = (event) => {
+        setNewZip(event.target.value)
+    }
+
+    const handleNewDescription = (event) => {
+        setNewDescription(event.target.value)
+    }
+
+    const handleNewImage = (event) => {
+        setNewImage(event.target.value)
+    }
+
+    const handleNewRating = (event) => {
+        setNewRating(event.target.value)
+    }
+
+    const handleNewTags = (event) => {
+        setNewTags(event.target.value)
+    }
+
+    // POST ROUTE
+    const handleNewLocationForm = (event) => {
+        event.preventDefault()
+        axios.post(
+            'https://project3-travelapp-backend.herokuapp.com/locations',
+            {
+                location: newLocation,
+                city: newCity,
+                zip: newZip,
+                description: newDescription,
+                image: newImage,
+                rating: newRating,
+                tags: newTags,
+            }
+        ).then(() => {
+            axios   
+                .get('https://project3-travelapp-backend.herokuapp.com/locations')
+                .then((response) => {
+                    props.setLocations(response.data)
+                })
+        })
+        event.target.reset()
+    }
+
     return (
         <div>
-            <form onSubmit={handleNewLocationForm}>
-                location: <input type="text" onChange={handleNewLocation} /><br />
-                city: <input type="text" onChange={handleNewCity} /><br />
-                zip: <input type="number" onChange={handleNewZip} /><br />
-                description: <input type="text" onChange={handleNewDescription} /><br />
-                image: <input type="text" onChange={handleNewImage} /><br />
-                rating: <input type="number" onChange={handleNewRating} /><br />
-                tags: <input type="text" onChange={handleNewTags} /><br />
-                <input type="submit" value="Add Location" />
-            </form>
+            <section className="collapse" id="formSection">
+                <form onSubmit={handleNewLocationForm}>
+                    location: <input type="text" className="form-control" onChange={handleNewLocation} /><br />
+                    city: <input type="text" className="form-control" onChange={handleNewCity} /><br />
+                    zip: <input type="number" className="form-control" onChange={handleNewZip} /><br />
+                    description: <input type="text" className="form-control" onChange={handleNewDescription} /><br />
+                    image: <input type="text" className="form-control" onChange={handleNewImage} /><br />
+                    rating: <input type="range" className="form-range" min="0" max="5" onChange={handleNewRating} /><br />
+                    tags: <input type="text" className="form-control" onChange={handleNewTags} /><br />
+                    <button className='btn btn-primary' data-bs-toggle="collapse" href={`#formSection`} type="submit">Submit Attraction</button>
+                </form>
+            </section>
         </div>
     )
   }
