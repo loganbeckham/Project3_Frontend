6567b2271584b84923aa078f51f0d88a725ca3c8
loganbeckham <loganbeckham1@gmail.com>
2022-12-14 15:48:40 -0600

    Basic Search Function Implemented

--- src/components/DisplayCards.js
+++ src/components/DisplayCards.js
@@ -5,13 +5,13 @@ const DisplayCards = (props) => {
 
     return (
         <>
-            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
-                <div className="card p-5" style={{minWidth: '350px'}}>
-                    <img className="card-img" src={props.location.image} alt="..." onClick={(event) => {props.cardDisplay(props.location)}}/>
-                    <div className='card-body' onClick={(event) => {props.cardDisplay(props.location)}}>
-                        <h5 className='card-title'>{props.location.location}</h5>
-                        <p className='card-text'>{props.location.city}</p>
-                        <p className='card-text'>{props.location.rating} Stars</p>
+            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 display-card-container">
+                <div className="card px-5 pt-5 h-100" style={{minWidth: '350px'}}>
+                    <img className="card-img" src={props.filteredResults.image} alt="..." onClick={(event) => {props.cardDisplay(props.filteredResults)}}/>
+                    <div className='card-body' onClick={(event) => {props.cardDisplay(props.filteredResults)}}>
+                        <h5 className='card-title'>{props.filteredResults.location}</h5>
+                        <p className='card-text'>{props.filteredResults.city}</p>
+                        <p className='card-text'>{props.filteredResults.rating} Stars</p>
                     </div>                                       
                 </div>
             </div>
